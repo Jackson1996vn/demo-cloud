68272d2a0736ff0131bcb0b9b5fe51a50388bdcb
Jackson.Nguyen <lastromeo1996bn@gmail.com>
2022-09-23 16:26:36 +0700

    add demo

--- main.tf
+++ main.tf
@@ -23,96 +23,124 @@ variable "account_id" {
   default = "364489674724"
 }
 
-resource "aws_iam_role" "lambda_role" {
+resource "aws_s3_bucket" "lambda_bucket" {
+  bucket = "lambda-hello-demo-by-jackson"
+}
+
+data "archive_file" "lambda_hello_world" {
+  type = "zip"
+
+  source_dir  = "${path.module}/hello-world"
+  output_path = "${path.module}/hello-world.zip"
+}
+
+resource "aws_s3_object" "lambda_hello_world" {
+  bucket = aws_s3_bucket.lambda_bucket.id
+
+  key    = "hello-world.zip"
+  source = data.archive_file.lambda_hello_world.output_path
+
+  etag = filemd5(data.archive_file.lambda_hello_world.output_path)
+}
+
+resource "aws_lambda_function" "hello_world" {
+  function_name = "HelloWorld"
+
+  s3_bucket = aws_s3_bucket.lambda_bucket.id
+  s3_key    = aws_s3_object.lambda_hello_world.key
+
+  runtime = "nodejs12.x"
+  handler = "hello.handler"
+
+  source_code_hash = data.archive_file.lambda_hello_world.output_base64sha256
+
+  role = aws_iam_role.lambda_exec.arn
+}
+
+resource "aws_cloudwatch_log_group" "hello_world" {
+  name = "/aws/lambda/${aws_lambda_function.hello_world.function_name}"
+
+  retention_in_days = 30
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "serverless_lambda"
+
   assume_role_policy = jsonencode({
-    Version : "2012-10-17",
-    Statement : [
-      {
-        Action : "sts:AssumeRole",
-        Principal : {
-          Service : "lambda.amazonaws.com"
-        },
-        Effect : "Allow",
-        Sid : ""
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Sid    = ""
+      Principal = {
+        Service = "lambda.amazonaws.com"
       }
+    }
     ]
   })
 }
-output "output_lambda_role" {
-  value = aws_iam_role.lambda_role.arn
-}
 
-resource "aws_iam_policy_attachment" "lambda_policy_attachment" {
-  name       = "lambda_policy_attachment"
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_exec.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-  roles      = [
-    aws_iam_role.lambda_role.name
-  ]
 }
 
-data "archive_file" "lambda_function_zip" {
-  output_path = "${path.module}/demo_function.zip"
-  type        = "zip"
-  source_dir = "${path.module}/backend-code/build"
+resource "aws_apigatewayv2_api" "lambda" {
+  name          = "serverless_lambda_gw"
+  protocol_type = "HTTP"
 }
 
-resource "aws_lambda_function" "lambda_function" {
-  function_name                  = "demo_function"
-  role                           = aws_iam_role.lambda_role.arn
-  handler                        = "index.handler"
-  runtime                        = "nodejs16.x"
-  timeout                        = 10
-  reserved_concurrent_executions = -1
-  filename                       = "demo_function.zip"
-}
+resource "aws_apigatewayv2_stage" "lambda" {
+  api_id = aws_apigatewayv2_api.lambda.id
 
-resource "aws_api_gateway_rest_api" "demo_api_gateway" {
-  name           = "demo_api_gateway"
-  api_key_source = "HEADER"
-}
+  name        = "serverless_lambda_stage"
+  auto_deploy = true
 
-resource "aws_api_gateway_resource" "root_resource" {
-  parent_id   = aws_api_gateway_rest_api.demo_api_gateway.root_resource_id
-  path_part   = "{proxy+}"
-  rest_api_id = aws_api_gateway_rest_api.demo_api_gateway.id
-}
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.api_gw.arn
 
-resource "aws_api_gateway_method" "demo_method" {
-  authorization      = "NONE"
-  http_method        = "ANY"
-  resource_id        = aws_api_gateway_resource.root_resource.id
-  rest_api_id        = aws_api_gateway_rest_api.demo_api_gateway.id
-  request_parameters = {
-    "method.request.path.proxy" = true
+    format = jsonencode({
+      requestId               = "$context.requestId"
+      sourceIp                = "$context.identity.sourceIp"
+      requestTime             = "$context.requestTime"
+      protocol                = "$context.protocol"
+      httpMethod              = "$context.httpMethod"
+      resourcePath            = "$context.resourcePath"
+      routeKey                = "$context.routeKey"
+      status                  = "$context.status"
+      responseLength          = "$context.responseLength"
+      integrationErrorMessage = "$context.integrationErrorMessage"
+    }
+    )
   }
 }
 
-resource "aws_api_gateway_integration" "demo_integrate" {
-  http_method = aws_api_gateway_method.demo_method.http_method
-  resource_id = aws_api_gateway_resource.root_resource.id
-  rest_api_id = aws_api_gateway_rest_api.demo_api_gateway.id
-  type        = "AWS_PROXY"
-  uri = aws_lambda_function.lambda_function.invoke_arn
-  integration_http_method = "POST"
+resource "aws_apigatewayv2_integration" "hello_world" {
+  api_id = aws_apigatewayv2_api.lambda.id
+
+  integration_uri    = aws_lambda_function.hello_world.invoke_arn
+  integration_type   = "AWS_PROXY"
+  integration_method = "POST"
 }
 
-resource "aws_api_gateway_deployment" "demo_apigw_deployment" {
-  rest_api_id = aws_api_gateway_rest_api.demo_api_gateway.id
+resource "aws_apigatewayv2_route" "hello_world" {
+  api_id = aws_apigatewayv2_api.lambda.id
+
+  route_key = "GET /hello"
+  target    = "integrations/${aws_apigatewayv2_integration.hello_world.id}"
 }
 
-resource "aws_api_gateway_stage" "demo_apigw_stage" {
-  deployment_id = aws_api_gateway_deployment.demo_apigw_deployment.id
-  rest_api_id   = aws_api_gateway_rest_api.demo_api_gateway.id
-  stage_name    = "dev"
+resource "aws_cloudwatch_log_group" "api_gw" {
+  name = "/aws/api_gw/${aws_apigatewayv2_api.lambda.name}"
+
+  retention_in_days = 30
 }
 
-resource "aws_lambda_permission" "apigw_lambda_permission" {
+resource "aws_lambda_permission" "api_gw" {
+  statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.lambda_function.function_name
+  function_name = aws_lambda_function.hello_world.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn = "arn:aws:execute-api:${var.region}:${var.account_id}:${aws_api_gateway_rest_api.demo_api_gateway.id}/*/${aws_api_gateway_method.demo_method.http_method}${aws_api_gateway_resource.root_resource.path}"
-}
 
-output "apigw_url" {
-  value = aws_api_gateway_stage.demo_apigw_stage.invoke_url
+  source_arn = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
 }
